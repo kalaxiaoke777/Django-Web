129b0da4f87b34a5e7f97075279ecdea6f22dba7
dengruizhe <2331602821@qq.com>
2024-03-05 23:26:05 +0800

    commit for addPoint

--- mysite/fishing/views.py
+++ mysite/fishing/views.py
@@ -1,10 +1,39 @@
+import json
+from shapely import wkt
 from django.shortcuts import render
-from django.http import HttpResponse, JsonResponse
+from django.http import HttpResponse, JsonResponse,HttpResponseNotAllowed
 from django.db import connections
 from django.views.decorators.csrf import csrf_exempt
 from django.contrib.gis.geos import GEOSGeometry
-from django.db import connection
-
+from django.contrib.gis.geos import Point
+def check_data(geom,spot_name):
+    """
+    检查数据是否符合中国坐标系标准
+    """
+    chiniGeometry = {
+        "min_E":73.5,
+        "max_E":135.0,
+        "min_N":15.5,
+        "max_N":55.0
+    }
+    # 创建Geometry对象
+    geom = wkt.loads(geom)
+    # 提取坐标
+    coordinates = geom.coords[0]
+    if (coordinates[0] > chiniGeometry["min_E"] and coordinates[0] < chiniGeometry["max_E"]) or (coordinates[1] > chiniGeometry["min_N"] and coordinates[1] < chiniGeometry["max_N"]):
+        return {
+        "type": True,
+        "message": ""
+    }
+    if spot_name is None or spot_name == '':
+        return {
+            "type": False,
+            "message": "spot_name不能为空"
+        }
+    return {
+        "type": True,
+        "message": ""
+    }
 @csrf_exempt 
 def addPoint(request):
     """
@@ -12,17 +41,57 @@ def addPoint(request):
     2.存入数据库
     3.刷新geoserver服务
     """
+    if request.method == 'POST':
+        # 解析数据
+        json_data = json.loads(request.body)
+        if "user_review" not in json_data:
+            user_review=""
+        else:
+            user_review=json_data["user_review"]
+        geom=json_data["geom"]
+        spot_name=json_data["spot_name"]
+        openning_time=json_data["openning_time"]
+        end_time=json_data["end_time"]
+        spot_area=json_data["spot_area"]
+        locations=json_data["locations"]
+        spot_type=json_data["spot_type"]
+        address=json_data["address"]
+        r_rating=json_data["r_rating"]
+        overview=json_data["overview"]
+        facilities=json_data["facilities"]
+        fish_active=json_data["fish_active"]
+        # 检查数据
+        check_result = check_data(geom,spot_name)
+        if check_result["type"] is False:
+            return JsonResponse(
+                        {
+                            "message": check_result["message"],
+                            "code": 400,
+                            "success": False,
+                        }
+                    )
+    else:
+        return HttpResponseNotAllowed(['POST'])
     with connections['fish'].cursor() as cursor:
         cursor.execute(
-                "SELECT * FROM fishing_user"
-            )
-        rows = cursor.fetchall()
-        for row in rows:
-            # 处理查询结果
-            print(row)
+            "INSERT INTO fishing_spots (geom, spot_name, openning_time, end_time, spot_area, locations, spot_type, address, user_review, r_rating, overview, facilities, fish_active) VALUES (ST_GeomFromText(%s, 4490), %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)",
+            [geom, 
+            spot_name, 
+            openning_time, 
+            end_time, 
+            spot_area, 
+            locations, 
+            spot_type, 
+            address, 
+            user_review, 
+            r_rating, 
+            overview, 
+            facilities, 
+            fish_active]
+        )
     return JsonResponse(
                     {
-                        "message": "查询成功",
+                        "message": "添加成功",
                         "code": 200,
                         "success": True,
                     }
